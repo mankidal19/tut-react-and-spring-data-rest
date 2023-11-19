626bfe667016f0636d87418dad08b06ce193ba8d
kakmon <man.kidal19@gmail.com>
2023-11-20 07:29:33 +0800

    move to UI subpkg

--- basic/src/main/js/ui/EmployeeUI.js
+++ basic/src/main/js/ui/EmployeeUI.js
@@ -1,36 +1,37 @@
 const React = require('react');
 
-export class EmployeeList extends React.Component {
-
-    render() {
-        const employees = this.props.employees.map(employee => {
-            <Employee key={employee._links.self.href} data={employee}/>
-        });
-
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Description</th>
-                    </tr>
-                    {employees}
-                </tbody>
-            </table>
-        );
-    }
+// tag::employee-list[]
+export class EmployeeList extends React.Component{
+	render() {
+		const employees = this.props.employees.map(employee =>
+			<Employee key={employee._links.self.href} data={employee}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Description</th>
+					</tr>
+					{employees}
+				</tbody>
+			</table>
+		);
+	}
 }
+// end::employee-list[]
 
-class Employee extends React.Component {
-
-    render() {
-        return (
-            <tr>
-                <td>{this.props.data.firstName}</td>
-                <td>{this.props.data.lastName}</td>
-                <td>{this.props.data.description}</td>
-            </tr>
-        );
-    }
+// tag::employee[]
+class Employee extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.data.firstName}</td>
+				<td>{this.props.data.lastName}</td>
+				<td>{this.props.data.description}</td>
+			</tr>
+		);
+	}
 }
+// end::employee[]
