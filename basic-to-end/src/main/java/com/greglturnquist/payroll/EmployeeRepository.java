22ea0d79ec21db41c443e20c353ca7e31d9484e6
kakmon <man.kidal19@gmail.com>
2023-11-20 08:01:24 +0800

    update repo class to extend PagingAndSortingRepository

--- basic-to-end/src/main/java/com/greglturnquist/payroll/EmployeeRepository.java
+++ basic-to-end/src/main/java/com/greglturnquist/payroll/EmployeeRepository.java
@@ -15,13 +15,13 @@
  */
 package com.greglturnquist.payroll;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * @author Greg Turnquist
  */
 // tag::code[]
-public interface EmployeeRepository extends CrudRepository<Employee, Long> { // <1>
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> { // <1>
 
 }
 // end::code[]
