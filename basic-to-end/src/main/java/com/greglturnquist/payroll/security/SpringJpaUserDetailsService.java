519d4720a484d591b6bc42743b0c205f5f9b7fd9
kakmon <man.kidal19@gmail.com>
2023-11-20 14:59:53 +0800

    move to security subpkg

--- basic-to-end/src/main/java/com/greglturnquist/payroll/security/SpringJpaUserDetailsService.java
+++ basic-to-end/src/main/java/com/greglturnquist/payroll/security/SpringJpaUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.greglturnquist.payroll;
+package com.greglturnquist.payroll.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
